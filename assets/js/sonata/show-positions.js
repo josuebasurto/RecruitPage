6b6e3231528d1f24b99abc2c51f3622904c3b7c5
Juan Fernandez Murrieta <juan.fernandez@sonatasmx.com>
2015-07-27 11:12:07 -0700

    Create new layout for job descriptions for china page

--- assets/js/sonata/show-positions.js
+++ assets/js/sonata/show-positions.js
@@ -1,18 +1,24 @@
 $("a.positions").on( "click", function( event ) {
-	var button = $(event.target);
-	var icon = $(button).children('i');
+	var click = $(event.target);
+	var icon = click.is('a') ? $(click).children('i') : click;
 	var jbBlock = $(event.target).closest("div.jb-block");
 	var positions = jbBlock.children('div.job-positions');
 	var hidden = positions.css('display');
+	var allPositions = $('.all-positions');
+	var icons = allPositions.find('i');
 
 	if (hidden == "none") {
 		$('div.job-positions').hide();
+		$(icons).each(function() {
+			$(this).removeClass('glyphicon-chevron-up');
+		    $(this).addClass('glyphicon-chevron-down');		    
+		});
 		positions.fadeIn();
-		icon.addClass('glyphicon-chevron-down');
-		icon.removeClass('glyphicon-chevron-up');
+		icon.addClass('glyphicon-chevron-up');
+		icon.removeClass('glyphicon-chevron-down');
 	} else {
 		positions.fadeOut();
-		icon.removeClass('glyphicon-chevron-down');
-		icon.addClass('glyphicon-chevron-up');		
+		icon.removeClass('glyphicon-chevron-up');
+		icon.addClass('glyphicon-chevron-down');		
 	}	
 });
